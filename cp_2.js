f53495112c5287362d8a61055dde4fcd896c6ed4
Olivia Thorpe <othorpe@usf.edu>
2026-03-23 15:02:52 -0400

    Implemented async/await, try/catch, displayProducts, handleError

--- cp_2.js
+++ cp_2.js
@@ -12,3 +12,13 @@ function fetchproductsThen() {
             console.error("Error fetching products:", error);
         });
 }
+
+async function fetchproductsAsync() {
+    try {
+        const response = await fetch("https://www.course-api.com/javascript-store-products");
+        const products = await response.json();
+        displayProducts(products);
+        catch (error) {
+            handleError(error);
+        }
+    }
