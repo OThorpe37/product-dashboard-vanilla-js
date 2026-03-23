5c6999ac9b96b28ad6b2d915c4e3d3ac28090f04
Olivia Thorpe <othorpe@usf.edu>
2026-03-23 15:56:20 -0400

    Adjusted camelCase and last edits

--- cp_2.js
+++ cp_2.js
@@ -1,11 +1,11 @@
-function fetchproductsThen() {
+function fetchProductsThen() {
     fetch("https://www.course-api.com/javascript-store-products")
         .then(function(response) {
             return response.json();
         })
         .then(function(products) {
             products.forEach(function(product) {
-                console.log(product);
+                console.log(product.fields.name);
             });
         })
         .catch(function(error) {
@@ -13,7 +13,7 @@ function fetchproductsThen() {
         });
 }
 
-async function fetchproductsAsync() {
+async function fetchProductsAsync() {
     try {
         const response = await fetch("https://www.course-api.com/javascript-store-products");
         const products = await response.json();
@@ -30,14 +30,14 @@ function displayProducts(products) {
     products.slice(0, 5).forEach(function(product) {
         const card = document.createElement("div");
         card.classList.add("product-card");
-        const name = product.name;
-        const image = product.image;
-        const price = product.price;
+        const name = product.fields.name;
+        const image = product.fields.image[0].url;
+        const price = product.fields.price;
 
         card.innerHTML = `
             <img src="${image}" alt="${name}">
             <h3>${name}</h3>
-            <p>Price: $${price / 100}.toFixed(2)</p>
+            <p>Price: $${(price / 100).toFixed(2)}</p>
         `;
         container.appendChild(card);
     });
@@ -47,5 +47,5 @@ function handleError(error) {
     console.error(`An error has occurred: ${error.message}`);
 }
 
-fetchproductsThen();
-fetchproductsAsync();
+fetchProductsThen();
+fetchProductsAsync();
